7b325e45cce29b7addf8443db924aab9db41443a
mahyuan <dsz9258@163.com>
2021-05-31 14:20:50 +0800

    wallpaper

--- wallpaper.sh
+++ wallpaper.sh
@@ -5,27 +5,27 @@
 # os x系统调整通知
 osascript -e 'display notification "Bing 爬虫开始执行" with title "通知" subtitle ""'
 
-flod_home="/Users/mhy/workspace/python_floder/python_demo/bing"
-flod_work="/Users/mhy/private/python_demo/bing"
-log="/Users/mhy/logs/wallpaper.log"
+user="$USER"
+script_flod="/Users/${user}/private/python_demo/bing"
+
+log="/Users/${user}/logs/wallpaper.log"
 
 date '+%Y'/'%m'/'%d %H':'%M':'%S'': wallpaper' | tee -a -i ${log}
 echo -e "\033[31m-----------开始执行爬取数据脚本了-----------\033[0m"
 
-if [ -d ${flod_home} ]; then
-    fld=${flod_home}
-elif [ -d ${flod_work} ]; then
-    fld=${flod_work}
+if [ -d ${script_flod} ]; then
+    echo "script flod is ${script_flod}"
 else
+    echo "flod not exists, ${script_flod}"
     exit 0
-    echo "not exists this floader"
 fi
 
+
 echo '开始爬取新数据........'
-python3 "${fld}/bing_picture.py"
+python3 "${script_flod}/bing_picture.py"
 if [ $? -eq 0 ]; then
     echo -e "\033[35m--------爬取数据成功了，现在下载图片-----------\033[0m"
-    python3 ${fld}'/dowload_picture.py'
+    python3 ${script_flod}'/dowload_picture.py'
 else
     echo  -e "\033[35m-----------爬取数据失败-----------\033[0m"
 fi
